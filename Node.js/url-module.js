f5f47b3fb83f6fd2c194a135db5f3b18cc1e9133
Jashandeep Kaur Sidhu <117754690+JashandeepSidhu712@users.noreply.github.com>
2023-09-10 22:17:19 +0530

    Update url-module.js

--- Node.js/url-module.js
+++ Node.js/url-module.js
@@ -15,6 +15,8 @@ var obj = qs.parse(query);
 console.log(obj);
 console.log(obj.uid);
 
+OUTPUT
+
 PS C:\Users\lcp\Dropbox\mernstack2023\nodejs> node url-module.js
 Url {
   protocol: 'http:',
@@ -33,4 +35,43 @@ Url {
 uid=jashan712&pwd=sidhu
 [Object: null prototype] { uid: 'jashan712', pwd: 'sidhu' }
 jashan712
+
+EXPLANATION
+
+uses the Node.js url and querystring modules to parse a URL and extract query parameters.
+
+var url = require("url");
+  This line imports the Node.js url module, which provides utilities for URL manipulation and parsing.
+
+var fullurl = "http://jashansidhu.com:80/users/open.html?uid=jashan712&pwd=sidhu";
+  This line defines a fullurl variable containing a URL as a string.
+
+var parsedUrl = url.parse(fullurl);
+  This line uses the url.parse() function to parse the fullurl and create a URL object. 
+    The URL object contains various properties like protocol, hostname, port, pathname, query, etc., representing different parts of the URL.
+
+console.log(parsedUrl);
+  This line prints the parsed URL object to the console, allowing you to see its structure and the individual components.
+
+var query = parsedUrl.query;
+  This line extracts the query string from the parsed URL object and stores it in the query variable.
+
+console.log(query);
+  This line prints the query string to the console.
+
+var qs = require("querystring");
+  This line imports the querystring module, which provides utilities for parsing and formatting URL query strings.
+
+var obj = qs.parse(query);
+  This line uses the qs.parse() function to parse the query string into a JavaScript object. 
+    It separates the query parameters and their values into key-value pairs within the object.
+
+console.log(obj);
+  This line prints the parsed object containing the query parameters and their values to the console.
+
+console.log(obj.uid);
+  This line prints the value associated with the "uid" query parameter from the obj object. 
+      In this case, it will output "jashan712" to the console.
+
+
 
